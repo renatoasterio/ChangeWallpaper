4aa36da2b684df9e13f0452575a272d2c0293881
Renato Asterio <renato.asterio@outlook.com>
2015-12-17 15:00:23 -0200

    first commit

---
+++ ChangeWallpaper/ChangeWallpaper.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace ChangeWallpaper
+{
+    partial class ChangeWallpaper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChangeWallpaper));
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBrower = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnApply = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtUrl
+            // 
+            resources.ApplyResources(this.txtUrl, "txtUrl");
+            this.txtUrl.Name = "txtUrl";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // btnBrower
+            // 
+            resources.ApplyResources(this.btnBrower, "btnBrower");
+            this.btnBrower.Name = "btnBrower";
+            this.btnBrower.UseVisualStyleBackColor = true;
+            this.btnBrower.Click += new System.EventHandler(this.btnBrower_Click);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Name = "label2";
+            // 
+            // btnApply
+            // 
+            resources.ApplyResources(this.btnApply, "btnApply");
+            this.btnApply.Name = "btnApply";
+            this.btnApply.UseVisualStyleBackColor = true;
+            this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
+            // 
+            // ChangeWallpaper
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnApply);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnBrower);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUrl);
+            this.MaximizeBox = false;
+            this.Name = "ChangeWallpaper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBrower;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnApply;
+    }
+}
+
---
+++ ChangeWallpaper/ChangeWallpaper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace ChangeWallpaper
+{
+    public partial class ChangeWallpaper : Form
+    {
+        public ChangeWallpaper()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnBrower_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Images (*.BMP;*.JPG;*.GIF,*.PNG,*.TIFF)|*.BMP;*.JPG;*.GIF;*.PNG;*.TIFF|" + "All files (*.*)|*.*";
+            dialog.CheckFileExists = true;
+            dialog.CheckPathExists = true;
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+                txtUrl.Text = dialog.FileName;
+        }
+
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern Int32 SystemParametersInfo(UInt32 uiAction, UInt32 uiParam, String pvParam, UInt32 fWinIni);
+
+        private void btnApply_Click(object sender, EventArgs e)
+        {
+            string path = txtUrl.Text;
+
+            if (string.IsNullOrEmpty(path))
+            {
+                MessageBox.Show("Plese, enter the URL.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            uint SPI_SETDESKWALLPAPER = 0x14;
+            uint SPIF_UPDATEINIFILE = 0x01;
+            uint SPIF_SENDWININICHANGE = 0x02;
+
+            Stream download = new System.Net.WebClient().OpenRead(path);
+            Image img = Image.FromStream(download);
+
+            string tempPath = Path.Combine(Path.GetTempPath(), "wallpaper.bmp");
+            img.Save(tempPath, System.Drawing.Imaging.ImageFormat.Bmp);
+
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 1, tempPath,
+            SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+
+            MessageBox.Show("Wallpaper changed successfully");
+        }
+    }
+}
